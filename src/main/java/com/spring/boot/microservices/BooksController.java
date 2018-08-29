66c4452fc5563ec8018d7367b6171c85bd476f11
Krishna <surekrrishna@gmail.com>
2018-08-29 23:14:38 +0530

    added spring-boot-devtools dependency to pom

--- src/main/java/com/spring/boot/microservices/BooksController.java
+++ src/main/java/com/spring/boot/microservices/BooksController.java
@@ -13,8 +13,8 @@ public class BooksController {
 	public List<Book> getBooks(){
 		List<Book> bookList = new ArrayList<>();
 		bookList.add(new Book(1,"The Secret", "Ronda Byrne"));
-		bookList.add(new Book(1,"Who Moved my Cheese", "Krishna"));
-		bookList.add(new Book(1,"Friends and Influence", "John"));		
+		bookList.add(new Book(2,"Who Moved my Cheese", "Krishna"));
+		bookList.add(new Book(3,"Friends and Influence", "John"));		
 		
 		return bookList;		
 	}
